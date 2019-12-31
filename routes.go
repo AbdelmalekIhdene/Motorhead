71bf26aa208f895871e49e4fa18532138c6d4cb1
Abdelmalek Ihdene <abdelmalek.ihdene@gmail.com>
2019-12-30 22:32:55 -0500

    Add profile image for every motorcycle

--- routes.go
+++ routes.go
@@ -5,11 +5,11 @@ import "github.com/gorilla/mux"
 func (srv *server) Routes() {
 	handler := mux.NewRouter()
 	handler.HandleFunc("/", srv.LogRequest(srv.HandleTemplate("templates/index.html"))).Methods("GET")
-	handler.HandleFunc("/selection", srv.LogRequest(srv.HandleTemplate("templates/selection.html"))).Methods("GET")
+	handler.HandleFunc("/motorcycles", srv.LogRequest(srv.HandleTemplate("templates/motorcycles.html"))).Methods("GET")
 	handler.HandleFunc("/scripts/index.js", srv.LogRequest(srv.ServeFile("templates/scripts/index.js"))).Methods("GET")
 	handler.HandleFunc("/stylesheets/index.css", srv.LogRequest(srv.ServeFile("templates/stylesheets/index.css"))).Methods("GET")
 	handler.HandleFunc("/stylesheets/minireset.css", srv.LogRequest(srv.ServeFile("templates/stylesheets/minireset.css"))).Methods("GET")
-	handler.HandleFunc("/scripts/selection.js", srv.LogRequest(srv.ServeFile("templates/scripts/selection.js"))).Methods("GET")
-	handler.HandleFunc("/stylesheets/selection.css", srv.LogRequest(srv.ServeFile("templates/stylesheets/selection.css"))).Methods("GET")
+	handler.HandleFunc("/scripts/motorcycles.js", srv.LogRequest(srv.ServeFile("templates/scripts/motorcycles.js"))).Methods("GET")
+	handler.HandleFunc("/stylesheets/motorcycles.css", srv.LogRequest(srv.ServeFile("templates/stylesheets/motorcycles.css"))).Methods("GET")
 	srv.Server.Handler = handler
 }

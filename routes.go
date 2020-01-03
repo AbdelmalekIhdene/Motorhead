a6fac590aeba5120a8cc0e52b2b6acf3a9fbc0f6
Abdelmalek Ihdene <abdelmalek.ihdene@gmail.com>
2020-01-03 18:58:25 -0500

    React Hello, World!

--- routes.go
+++ routes.go
@@ -8,6 +8,7 @@ func (srv *server) Routes() {
 	handler.HandleFunc("/selection", srv.LogRequest(srv.HandleStaticTemplate("static/selection.html"))).Methods("GET")
 	handler.HandleFunc("/motorcycle", srv.LogRequest(srv.HandleMotorcycleTemplate("static/motorcycle.html"))).Methods("GET")
 	handler.HandleFunc("/scripts/{filename}", srv.LogRequest(srv.ServeDirectory("static/scripts"))).Methods("GET")
+	handler.HandleFunc("/components/{filename}", srv.LogRequest(srv.ServeDirectory("static/components"))).Methods("GET")
 	handler.HandleFunc("/stylesheets/{filename}", srv.LogRequest(srv.ServeDirectory("static/stylesheets"))).Methods("GET")
 	handler.HandleFunc("/images/hero/{filename}", srv.LogRequest(srv.ServeDirectory("static/images/hero"))).Methods("GET")
 	handler.HandleFunc("/images/sideview/{filename}", srv.LogRequest(srv.ServeDirectory("static/images/sideview"))).Methods("GET")
---
+++ static/components/selection.js
@@ -0,0 +1,7 @@
+class Selection extends React.Component {
+	render() {
+		return <h1>Hello, World!</h1>
+	}
+}
+ReactDOM.render(<Selection/>,
+document.getElementById("selection"));
